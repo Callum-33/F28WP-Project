947f4136936850d4b872c85da620ffcd24b57970
nhogg <nathanhogg1223@gmail.com>
2025-11-03 13:15:49 +0000

    Update server.js with routes, add hashing logic.

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 });
+
+const authRoutes = require('./routes/authRoutes');
+const listingRoutes = require('./routes/listingRoutes');
+
+app.use('/api', authRoutes);
+app.use('/api', listingRoutes);
